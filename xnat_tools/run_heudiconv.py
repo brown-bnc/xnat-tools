189b755a6627a3bcfc36db12c995f13aa2163353
Isabel Restrepo <isabelrestre@gmail.com>
2020-06-29 13:48:12 -0400

    feat: check dicom extension, add formatting

--- xnat_tools/run_heudiconv.py
+++ xnat_tools/run_heudiconv.py
@@ -1,11 +1,9 @@
-
-
-
 import subprocess
 import sys
 import argparse
 import shlex
 import shutil
+import glob
 from pathlib import Path
 from xnat_tools.xnat_utils import *
 from xnat_tools.bids_utils import *
@@ -21,43 +19,37 @@ def parse_args(args):
       :obj:`argparse.Namespace`: command line parameters namespace
     """
     parser = argparse.ArgumentParser(
-        description="Run Heudiconv on DICOMS form XNAT dump using Reproin Heuristic")
+        description="Run Heudiconv on DICOMS form XNAT dump using Reproin Heuristic"
+    )
+    parser.add_argument("--host", default="https://bnc.brown.edu/xnat", help="Host")
+    parser.add_argument("-u", "--user", help="XNAT username", required=True)
     parser.add_argument(
-        "--host",
-        default="https://bnc.brown.edu/xnat",
-        help="Host")
-    parser.add_argument(
-        "-u", "--user",
-        help="XNAT username",
-        required=True)
-    parser.add_argument(
-        '-p', '--password',
+        "-p",
+        "--password",
         type=XNATPass,
-        help='XNAT password',
-        default=XNATPass.DEFAULT)
-    parser.add_argument(
-        "--session",
-        help="Session ID",
-        required=True)
+        help="XNAT password",
+        default=XNATPass.DEFAULT,
+    )
+    parser.add_argument("--session", help="Session ID", required=True)
     parser.add_argument(
         "--session_suffix",
         help="Suffix of the session for BIDS e.g, 01. This will produce a sesstion label of sess-01",
         required=True,
-        type=str)
+        type=str,
+    )
     parser.add_argument(
-        "--bids_root_dir",
-        help="Root output directory for BIDS files",
-        required=True)
+        "--bids_root_dir", help="Root output directory for BIDS files", required=True
+    )
     parser.add_argument(
         "--cleanup",
         help="Remove/mode files and folders outside the bids directory",
-        action='store_true',
-        default=False)
+        action="store_true",
+        default=False,
+    )
 
     args, _ = parser.parse_known_args(args)
     return args
 
-  
 
 def main(args):
     """Main entry point allowing external calls
@@ -75,24 +67,37 @@ def main(args):
 
     # Set up working directory
     if not os.access(bids_root_dir, os.R_OK):
-        raise ValueError('BIDS Root directory must exist')
+        raise ValueError("BIDS Root directory must exist")
 
     # Set up session
     connection = requests.Session()
     connection.verify = False
     connection.auth = (args.user, args.password)
-    
+
     project, subject = get_project_and_subject_id(connection, host, session)
     connection.delete(f"{host}/data/JSESSION")
     connection.close()
 
-    #get PI from project name
-    investigator = project.lower().split('_')[0] 
+    # get PI from project name
+    investigator = project.lower().split("_")[0]
 
     # Paths to export source data in a BIDS friendly way
-    pi_prefix, study_prefix, subject_prefix, session_prefix = prepare_heudi_prefixes(project, subject, session_suffix)
+    pi_prefix, study_prefix, subject_prefix, session_prefix = prepare_heudi_prefixes(
+        project, subject, session_suffix
+    )
+    heudi_output_dir = prepare_heudiconv_output_path(
+        bids_root_dir, pi_prefix, study_prefix, subject_prefix, session_prefix
+    )
+    dicom_dir_template = f"{bids_root_dir}/{pi_prefix}/{study_prefix}/xnat-export/sub-{{subject}}/ses-{{session}}/"
 
-    heudi_output_dir = prepare_heudiconv_output_path(bids_root_dir, pi_prefix, study_prefix, subject_prefix, session_prefix)
+    # check if the extension of the images is dcm or IMA
+    dicom_ext = "dcm"
+
+    if len(glob.glob("{dicom_dir_template}/*.dcm")) == 0:
+        if len(glob.glob("{dicom_dir_template}/*.IMA")) > 0:
+            dicom_ext = "IMA"
+        else:
+            EnvironmentError(f"No .dcm or .IMA files found in {dicom_dir_template}")
 
     heudi_cmd = f"heudiconv -f reproin --bids \
     -o {heudi_output_dir} \
@@ -100,16 +105,19 @@ def main(args):
     --subjects {subject_prefix} --ses {session_prefix}"
 
     heudi_split_cmd = shlex.split(heudi_cmd)
-    
+
     print(f"Executing Heudiconv command: {heudi_cmd}")
 
-    stdout_file = open(str(Path(heudi_output_dir).parent) + "/logs/heudiconv_stdout.log", 'a')
-    stderr_file = open(str(Path(heudi_output_dir).parent) + "/logs/heudiconv_stderr.log", 'a') 
-    
-    process = subprocess.run(heudi_split_cmd, 
-                        stdout = stdout_file,
-                        stderr = stderr_file,
-                        universal_newlines = True)
+    stdout_file = open(
+        str(Path(heudi_output_dir).parent) + "/logs/heudiconv_stdout.log", "a"
+    )
+    stderr_file = open(
+        str(Path(heudi_output_dir).parent) + "/logs/heudiconv_stderr.log", "a"
+    )
+
+    process = subprocess.run(
+        heudi_split_cmd, stdout=stdout_file, stderr=stderr_file, universal_newlines=True
+    )
     # process = subprocess.run(heudi_split_cmd)
 
     stdout_file.close()
@@ -122,10 +130,11 @@ def main(args):
         print("Moving XNAT export log to derivatives folder")
 
         # check if directory exists or not yet
-        derivatives_dir = f"{bids_root_dir}/{pi_prefix}/{study_prefix}/bids/derivatives/xnat/logs"
+        derivatives_dir = (
+            f"{bids_root_dir}/{pi_prefix}/{study_prefix}/bids/derivatives/xnat/logs"
+        )
         if not os.path.exists(derivatives_dir):
             os.mkdir(derivatives_dir)
-
 
 
 def run():
