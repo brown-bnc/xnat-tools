a53d73faa4757da5f601ff6ab45ba106367c4377
Isabel Restrepo <isabelrestre@gmail.com>
2020-09-23 15:51:44 -0400

    feat: transition posprocessing to typer

--- xnat_tools/logging.py
+++ xnat_tools/logging.py
@@ -4,7 +4,7 @@ import sys
 
 
 def setup_logging(
-    logger, logfile: str, verbose: bool = False, very_verbose: bool = False
+    logger, logfile: str = "", verbose: bool = False, very_verbose: bool = False
 ):
     """Setup basic logging
 
@@ -16,10 +16,16 @@ def setup_logging(
     if very_verbose:
         loglevel = logging.DEBUG
     logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    handlers = None
+    if logfile == "":
+        handlers = [logging.StreamHandler(sys.stdout)]
+    else:
+        handlers = [logging.FileHandler(logfile), logging.StreamHandler(sys.stdout)]
+
     logging.basicConfig(
         level=loglevel,
         format=logformat,
         datefmt="%Y-%m-%d %H:%M:%S",
-        handlers=[logging.FileHandler(logfile), logging.StreamHandler(sys.stdout)],
+        handlers=handlers,
     )
     coloredlogs.install(level=loglevel, logger=logger)
