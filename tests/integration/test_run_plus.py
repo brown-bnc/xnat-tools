b52ed00d248c94b8d3528a0d1796097e9b11c9d4
Isabel Restrepo <isabelrestre@gmail.com>
2020-09-23 16:13:30 -0400

    tests: update run_plus to new syntax

--- tests/integration/test_run_plus.py
+++ tests/integration/test_run_plus.py
@@ -3,39 +3,41 @@ import os
 import shutil
 import shlex
 import pytest
+from typer.testing import CliRunner
+from xnat_tools.xnat2bids import app as app
 from dotenv import load_dotenv
 
 load_dotenv()
+runner = CliRunner()
 
 
-@pytest.mark.skip(reason="Another slow test")
+# @pytest.mark.skip(reason="Another slow test")
 def test_run_plus():
     """Integration test for sequence list"""
     xnat_user = os.environ.get("XNAT_USER", "testuser")
     xnat_pass = os.environ.get("XNAT_PASS", "")
-    session = os.environ.get("XNAT_SESSION", "XNAT2_E00004")
-    session_suffix = os.environ.get("XNAT_SESSION_SUFFIX", "01")
+    session = "XNAT2_E00004"
+    session_suffix = "01"
     bids_root_dir = os.environ.get("XNAT_BIDS_ROOT", "./tests/xnat2bids")
-    sequence = [8, 12, 15]
+    seqlist = ["8", "12", "15"]
 
     if os.path.exists(bids_root_dir):
         shutil.rmtree(bids_root_dir, ignore_errors=True)
 
     os.mkdir(bids_root_dir)
 
-    xnat2bids_cmd = f"xnat2bids --user {xnat_user} --password {xnat_pass} \
-                      --session {session} --session_suffix {session_suffix} \
-                      --bids_root_dir {bids_root_dir} --seqlist {' '.join(map(str, sequence))} -vv"
+    xnat2bids_cmd = f"{session} {bids_root_dir} -u {xnat_user} -p {xnat_pass} \
+                      -i {' -i '.join(seqlist)} --vv"
 
     xnat2bids_split_cmd = shlex.split(xnat2bids_cmd)
 
-    cmd = subprocess.run(xnat2bids_split_cmd, check=True)
+    r = runner.invoke(app, xnat2bids_split_cmd)
 
     xnat_export_path = f"tests/xnat2bids/shenhav/study-201226/xnat-export/sub-tcb2006/ses-{session_suffix}/"
     task_name = "func-bold_task-TSSblock_acq-2dot4mm-SMS4TR1200AP_run"
 
     assert os.path.isdir(os.path.join(os.getcwd(), xnat_export_path))
-    for i in range(1, len(sequence) + 1):
+    for i in range(1, len(seqlist) + 1):
         assert os.path.isdir(
             os.path.join(os.getcwd(), xnat_export_path, f"{task_name}-{i:02}")
         )
