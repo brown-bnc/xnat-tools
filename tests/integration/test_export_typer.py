2696874f7b03d04afab42bd4ec23a8acc1e4813d
Isabel Restrepo <isabelrestre@gmail.com>
2020-09-23 12:57:26 -0400

    tests(run-heudiconv): activate tests for typer version

--- tests/integration/test_export_typer.py
+++ tests/integration/test_export_typer.py
@@ -17,6 +17,8 @@ def test_dicom_export():
     """Integration test for xnat-dicom-export"""
     xnat_user = os.environ.get("XNAT_USER", "testuser")
     xnat_pass = os.environ.get("XNAT_PASS", "")
+    project = os.environ.get("XNAT_PROJECT", "")
+    subject = os.environ.get("XNAT_SUBJECT", "")
     session = os.environ.get("XNAT_SESSION", "")
     session_suffix = os.environ.get("XNAT_SESSION_SUFFIX", "01")
     bids_root_dir = os.environ.get("XNAT_BIDS_ROOT", "./tests/xnat2bids")
@@ -33,7 +35,7 @@ def test_dicom_export():
     # See https://github.com/tiangolo/typer/issues/106
     # ***************************************************************************
 
-    project, participant, = dicom_export(
+    p, s, = dicom_export(
         session,
         bids_root_dir,
         user=xnat_user,
@@ -49,8 +51,8 @@ def test_dicom_export():
         overwrite=False,
     )
 
-    assert project == "SANES_SADLUM"
-    assert participant == "123"
+    assert p == project
+    assert s == subject
 
     filepath = glob.glob(
         f"tests/xnat2bids/*/study-*/xnat-export/sub-*/ses-{session_suffix}"
@@ -116,58 +118,56 @@ def test_dicom_export():
             assert str(dicom_sequence) == "8"
 
 
-# def test_heudiconv():
-#     """Integration test for running xnat-heudiconv executable on the output of xnat-dicom-export """
-#     xnat_user = os.environ.get("XNAT_USER", "testuser")
-#     xnat_pass = os.environ.get("XNAT_PASS", "")
-#     session = os.environ.get("XNAT_SESSION", "")
-#     session_suffix = os.environ.get("XNAT_SESSION_SUFFIX", "01")
-#     bids_root_dir = os.environ.get("XNAT_BIDS_ROOT", "./tests/xnat2bids")
+def test_heudiconv():
+    """Integration test for running run-heudiconv executable on the output of xnat-dicom-export """
+    xnat_user = os.environ.get("XNAT_USER", "testuser")
+    xnat_pass = os.environ.get("XNAT_PASS", "")
+    project = os.environ.get("XNAT_PROJECT", "")
+    subject = os.environ.get("XNAT_SUBJECT", "")
+    session = os.environ.get("XNAT_SESSION", "")
+    session_suffix = os.environ.get("XNAT_SESSION_SUFFIX", "01")
+    bids_root_dir = os.environ.get("XNAT_BIDS_ROOT", "./tests/xnat2bids")
 
-#     # ***************************************************************************
-#     # Test for succesfull execution
-#     # ***************************************************************************
+    # ***************************************************************************
+    # Test for succesfull execution
+    # ***************************************************************************
+    cmd = f"run-heudiconv {project} {subject} {session} {bids_root_dir}"
 
-#     cmd = f"xnat-heudiconv --user {xnat_user} --password {xnat_pass} \
-#             --session {session} --session_suffix {session_suffix} \
-#             --bids_root_dir {bids_root_dir}"
+    split_cmd = shlex.split(cmd)
 
-#     split_cmd = shlex.split(cmd)
+    r = runner.invoke(app, split_cmd)
+    print(r.stdout)
 
-#     subprocess.run(split_cmd)
+    filepath = glob.glob(f"tests/xnat2bids/*/study-*/bids/sub-*/ses-{session_suffix}")[
+        0
+    ]
 
-#     filepath = glob.glob(f"tests/xnat2bids/*/study-*/bids/sub-*/ses-{session_suffix}")[
-#         0
-#     ]
+    assert (
+        len(glob.glob(f"{filepath}/*/*.json")) > 0
+        or len(glob.glob(f"{filepath}/*/*.nii.gz")) > 0
+    )
 
-#     assert (
-#         len(glob.glob(f"{filepath}/*/*.json")) > 0
-#         or len(glob.glob(f"{filepath}/*/*.nii.gz")) > 0
-#     )
+    # ***************************************************************************
+    # Test for RuntimeError Heudiconv doesn't allow overwrite
+    # ***************************************************************************
+    cmd = f"run-heudiconv {project} {subject} {session} {bids_root_dir}"
 
-#     # ***************************************************************************
-#     # Test for RuntimeError Heudiconv doesn't allow overwrite
-#     # ***************************************************************************
-#     cmd = f"xnat-heudiconv --user {xnat_user} --password {xnat_pass} \
-#             --session {session} --session_suffix {session_suffix} \
-#             --bids_root_dir {bids_root_dir}"
+    split_cmd = shlex.split(cmd)
+    r = runner.invoke(app, split_cmd)
+    print(r.stdout)
 
-#     split_cmd = shlex.split(cmd)
-#     p = subprocess.run(split_cmd)
+    assert r.exit_code == 1
 
-#     assert p.returncode == 1
+    # ***************************************************************************
+    # Test overwrite is working
+    # ***************************************************************************
 
-#     # ***************************************************************************
-#     # Test overwrite is working
-#     # ***************************************************************************
+    cmd = f"run-heudiconv {project} {subject} {session} {bids_root_dir} --overwrite"
 
-#     cmd = f"xnat-heudiconv --user {xnat_user} --password {xnat_pass} \
-#             --session {session} --session_suffix {session_suffix} \
-#             --bids_root_dir {bids_root_dir} --overwrite"
+    split_cmd = shlex.split(cmd)
+    p = runner.invoke(app, split_cmd)
+    print(r.stdout)
+    assert p.exit_code == 0
 
-#     split_cmd = shlex.split(cmd)
-#     p = subprocess.run(split_cmd)
-#     assert p.returncode == 0
-
-#     # cleanup output -- for debugging coment this out
-#     shutil.rmtree(bids_root_dir, ignore_errors=True)
+    cleanup output -- for debugging coment this out
+    shutil.rmtree(bids_root_dir, ignore_errors=True)
