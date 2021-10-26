c82ec766374bd51d0a3ede15352e2917cf473b08
Isabel Restrepo <isabelrestre@gmail.com>
2021-10-26 18:33:04 -0400

    chore: adjust xnat2bids test for demodat

--- tests/integration/test_xnat2bids.py
+++ tests/integration/test_xnat2bids.py
@@ -27,14 +27,12 @@ def series_idx(f):
 def test_xnat2bids():
     """Integration test for xnat2bids executable"""
 
-    xnat_user = os.environ.get("XNAT_USER", "testuser")
+    xnat_user = os.environ.get("XNAT_USER", "")
     xnat_pass = os.environ.get("XNAT_PASS", "")
     session = os.environ.get("XNAT_SESSION", "")
     session_suffix = os.environ.get("XNAT_SESSION_SUFFIX", "01")
     bids_root_dir = os.environ.get("XNAT_BIDS_ROOT", "./tests/xnat2bids")
-    bidsmap_file = os.environ.get("XNAT_BIDSMAP", "./tests/demodat_bidsmap.json")
-    seqlist = ["1", "2", "3", "7"]
-    skiplist = ["2", "3"]
+    skiplist = ["6"]
 
     if os.path.exists(bids_root_dir):
         shutil.rmtree(bids_root_dir, ignore_errors=True)
@@ -42,8 +40,7 @@ def test_xnat2bids():
     os.mkdir(bids_root_dir)
 
     xnat2bids_cmd = f"{session} {bids_root_dir} -u {xnat_user} -p {xnat_pass} \
-                      -i {' -i '.join(seqlist)} -s {' -s '.join(skiplist)} \
-                      -f {bidsmap_file}"
+                      -s {' -s '.join(skiplist)}"
 
     xnat2bids_split_cmd = shlex.split(xnat2bids_cmd)
     print(xnat2bids_split_cmd)
@@ -55,13 +52,13 @@ def test_xnat2bids():
     filepath = f"tests/xnat2bids/*/study-*/bids/sub-*/ses-{session_suffix}"
 
     # ***************************************************************************
-    # Test for succesfull execution
+    # Test for successful execution
     # ***************************************************************************
     assert len(glob.glob(f"{filepath}/*/*.json")) > 0
     assert len(glob.glob(f"{filepath}/*/*.nii.gz")) > 0
 
     # ***************************************************************************
-    # Test for correctess of sequence number given by seqlist and skiplist
+    # Test for correctess of sequence number given by skiplist
     # ***************************************************************************
     xnat_export_path = glob.glob(
         f"tests/xnat2bids/*/study-*/xnat-export/sub-*/ses-{session_suffix}"
@@ -75,7 +72,6 @@ def test_xnat2bids():
         for f in os.listdir(d):
             dataset = pydicom.dcmread(os.path.join(d, f))
             idx = series_idx(f)
-            assert str(idx) in seqlist
             assert str(idx) not in skiplist
             assert dataset.data_element("ProtocolName").value == series_desc
             assert dataset.data_element("SeriesDescription").value == series_desc
