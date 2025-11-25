4a1801b4303d3532b92b6ed46794e624ede9204f
Elizabeth Lorenc <elizabeth_lorenc@brown.edu>
2025-11-25 11:43:53 -0500

    feat: filter directories for heudiconv
    Instead of passing the entire xnat export DICOM directory to heudiconv, first filter out any scanner derivatives, etc. that we know will crash heudiconv. Then copy those DICOMs to bids/sourcedata just in case they're needed in the future.

--- xnat_tools/run_heudiconv.py
+++ xnat_tools/run_heudiconv.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import re
 import shlex
 import shutil
 import sys
@@ -8,6 +9,8 @@ from pathlib import Path
 from subprocess import PIPE, Popen
 
 import typer
+from heudiconv.dicoms import compress_dicoms
+from heudiconv.utils import TempDirs
 
 from xnat_tools.bids_utils import (
     path_string_preprocess,
@@ -71,33 +74,81 @@ def run_heudiconv(
     )
 
     export_dir = f"{bids_root_dir}/{pi_prefix}/{study_prefix}/xnat-export"
-    dicom_dir_template = f"{export_dir}/{subject_prefix}/{session_prefix}"
+    dicom_dir = Path(export_dir) / subject_prefix / session_prefix
 
     # check if the extension of the images is dcm or IMA
-    dicom_ext = "dcm"
-    if len(glob.glob(f"{dicom_dir_template}/*/*.dcm")) == 0:
-        if len(glob.glob(f"{dicom_dir_template}/*/*.IMA")) > 0:
-            dicom_ext = "IMA"
-        else:
-            raise ValueError(f"No .dcm or .IMA files found in {dicom_dir_template}")
+    if (len(glob.glob(f"{dicom_dir}/*/*.dcm")) == 0) and len(
+        glob.glob(f"{dicom_dir}/*/*.IMA")
+    ) == 0:
+        raise ValueError(f"No .dcm or .IMA files found in {dicom_dir}")
 
-    heudi_cmd = f"heudiconv -f reproin --bids \
-    -o {heudi_output_dir} \
-    --dicom_dir_template {export_dir}/sub-{{subject}}/ses-{{session}}/*/*.{dicom_ext} \
-    --subjects {subject} --ses {session_suffix}"
+    # all first-level series directories under the session
+    series_dirs = [d for d in dicom_dir.iterdir() if d.is_dir()]
+
+    # filter specific scan types / scanner derivatives that we know will break heudiconv
+    # some of these we will handle ourselves afterward
+    heudi_skip_list = [
+        "_MPR_",
+        "_ND",
+        "rf_map",
+        "tb1tfl",
+        "TB1TFL",
+        "_TRACEW",
+        "_TENSOR",
+        "_B0",
+        "_ColFA",
+        "_ADC",
+    ]
+
+    # split off bottom 3 levels because they need to be supplied as a locator path
+    heudi_base_dir = Path(heudi_output_dir).parents[3]
+    heudi_locator = Path(heudi_output_dir).relative_to(heudi_base_dir)
+
+    keep_dirs, skip_dirs = [], []
+
+    for d in series_dirs:
+        if any(s in d.name for s in heudi_skip_list):
+            skip_dirs.append(d)
+        else:
+            keep_dirs.append(d)
+
+    print(f"Skipping heudiconv BIDS conversion for {', '.join(s.name for s in skip_dirs)}")
+    print(f"DICOMS for those scans can be found in {str(heudi_output_dir)}/sourcedata")
+
+    if not keep_dirs:
+        raise RuntimeError(
+            f"No DICOM directories left after filtering XNAT export directory {dicom_dir}"
+        )
+
+    # Build heudiconv command using --files option, which will still look within directories
+    heudi_cmd = [
+        "heudiconv",
+        "-f",
+        "reproin",
+        "--bids",
+        "-o",
+        str(heudi_base_dir),
+        "--files",
+        *[str(d) for d in keep_dirs],
+        "--locator",
+        str(heudi_locator),
+        "--subjects",
+        subject,
+        "--ses",
+        session_suffix,
+    ]
 
     if overwrite:
-        heudi_cmd = heudi_cmd + " --overwrite"
+        heudi_cmd.append("--overwrite")
 
-    heudi_split_cmd = shlex.split(heudi_cmd)
+    print("Executing Heudiconv command:", " ".join(shlex.quote(a) for a in heudi_cmd))
 
-    print(f"Executing Heudiconv command: {heudi_cmd}")
+    logdir = str(Path(heudi_output_dir).parent) + "/logs"
+    logfile = f"{logdir}/heudiconv-{log_id}.log"
 
-    logfile = str(Path(heudi_output_dir).parent) + f"/logs/heudiconv-{log_id}.log"
-
-    with Popen(
-        heudi_split_cmd, stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True
-    ) as p, open(logfile, "a", encoding="utf-8") as file:
+    with Popen(heudi_cmd, stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True) as p, open(
+        logfile, "a", encoding="utf-8"
+    ) as file:
         stdout, stderr = p.communicate()
         sys.stdout.write(stdout)
         file.write(stdout)
@@ -106,7 +157,24 @@ def run_heudiconv(
         if p.returncode != 0:
             raise RuntimeError(f"Heudiconv failed with exit code {p.returncode}:\n{stderr}")
 
-    print("Done with Heudiconv BIDS Convesion.")
+    # copy over any skipped DICOMs into the bids/sourcedata/subject/session directory
+    tempdirs = TempDirs()
+    sourcedata_dir = f"{heudi_output_dir}/sourcedata/sub-{subject}/ses-{session_suffix}/"
+    bids_datatypes = ["anat", "func", "dwi", "fmap", "perf", "mrs"]
+
+    for s in skip_dirs:
+        dirname = re.split("-|_", s.name)[0]
+        if dirname in bids_datatypes:
+            pass
+        else:
+            dirname = "unknown"
+        if not Path(sourcedata_dir, dirname).exists():
+            os.makedirs(str(Path(sourcedata_dir, dirname)))
+        compress_dicoms(
+            glob.glob(f"{str(s)}/*"), f"{sourcedata_dir}{dirname}/{s.name}", tempdirs, False
+        )
+
+    print("Done with Heudiconv BIDS conversion.")
 
     if cleanup:
         print("Removing XNAT export.")
